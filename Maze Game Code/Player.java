5f81adf4023fd28a0a0da688f3d83295735856f0
RoxieTheWeirdo <146688788+RoxieTheWeirdo@users.noreply.github.com>
2024-04-04 14:57:40 -0500

    Item Menu Update 4/4/24

--- Maze Game Code/Player.java
+++ Maze Game Code/Player.java
@@ -1,17 +1,28 @@
 public class Player {
-    public double health;
-    public double stamina;
-    public int currentRow;
-    public int currentCol;
-    public int LVL;
-    public int currentLvl() {
-        return LVL;
-    }
+    double health;
+    double stamina;
+    int currentRow;
+    int currentCol;
+    int LVL;
+    String item1;
+    String item2;
     public void setBaseStats() {            //default stats loaded at beginning of program
-        health = 100;
+        health = 20;
         stamina = 100;
-        currentRow = 14;
-        currentCol = 1;
-        LVL = 1;
+        currentRow = 0;
+        currentCol = 0;
+        LVL = 2;
+        item1 ="";
+        item2 ="";
+    }
+    public void basePosition(Player Pl, int LVL) {
+        if (LVL == 1) {
+            Pl.currentRow = 14;
+            Pl.currentCol = 1;
+        }
+        else if (LVL == 2) {
+            Pl.currentRow = 0;
+            Pl.currentCol = 0;
+        }
     }
 }
