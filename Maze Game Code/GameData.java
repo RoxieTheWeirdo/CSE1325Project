12d924991df4c61b85f1b73e55c134b95c1bfe35
RoxieTheWeirdo <146688788+RoxieTheWeirdo@users.noreply.github.com>
2024-04-04 14:58:08 -0500

    Item Menu Update 4/4/24

--- Maze Game Code/GameData.java
+++ Maze Game Code/GameData.java
@@ -1,28 +1,109 @@
-public class Player {
-    double health;
-    double stamina;
-    int currentRow;
-    int currentCol;
-    int LVL;
-    String item1;
-    String item2;
-    public void setBaseStats() {            //default stats loaded at beginning of program
-        health = 20;
-        stamina = 100;
-        currentRow = 0;
-        currentCol = 0;
-        LVL = 2;
-        item1 ="";
-        item2 ="";
+public class GameData {
+        public String BlueColor = "\u001B[34m";
+        //public String RedColor = "\u001B[31m";
+        public String ResetColor = "\u001B[0m";
+    public boolean passable(char[][] maze, Player Pl,int offsetRow, int offsetCol) {
+        if (maze[Pl.currentRow + offsetRow][Pl.currentCol + offsetCol] == '█') {        //Basic Wall
+            System.out.println("Invalid Position! (You'd take damage normally)");       //can modify more blocks later (breakable, ball, etc)
+            return false;
+        }
+        else if (maze[Pl.currentRow + offsetRow][Pl.currentCol + offsetCol] == '▓') {   //Breakable Wall
+            System.out.println("Invalid Position! (You'd take damage normally)");
+            return false;
+        }
+        else if (maze[Pl.currentRow + offsetRow][Pl.currentCol + offsetCol] == 'X') {   //Key Door
+            System.out.println("Invalid Position! (You'd take damage normally)");
+            return false;
+        }
+        return true;
     }
-    public void basePosition(Player Pl, int LVL) {
-        if (LVL == 1) {
-            Pl.currentRow = 14;
-            Pl.currentCol = 1;
+    public void useItem(Player Pl, String Item, char[][] currentMaze, int itemSlot) {
+        switch (Item) {
+            case "Potion of Vigor":
+                if (Pl.health < 100) {
+                    Pl.health += 50;
+                    if (Pl.health > 100) {
+                        Pl.health = 100;
+                    }
+                }
+                else {
+                    System.out.println("Using a Potion of Vigor now would have no effect!");
+                    return;
+                }
+                break;
+            case "Key":
+                if (Pl.currentCol + 1 < currentMaze[0].length && currentMaze[Pl.currentRow][Pl.currentCol+1] == 'X') {
+                    currentMaze[Pl.currentRow][Pl.currentCol+1] = '-';
+                    System.out.println("You have unlocked a door!");
+                }
+                else if (Pl.currentRow + 1 < currentMaze[0].length && currentMaze[Pl.currentRow+1][Pl.currentCol] == 'X') {
+                    currentMaze[Pl.currentRow+1][Pl.currentCol] = '-';
+                    System.out.println("You have unlocked a door!");
+                }
+                else if (Pl.currentRow - 1 >= 0 && currentMaze[Pl.currentRow][Pl.currentCol-1] == 'X') {
+                    currentMaze[Pl.currentRow][Pl.currentCol-1] = '-';
+                    System.out.println("You have unlocked a door!");
+                }
+                else if (Pl.currentCol - 1 >= 0 && currentMaze[Pl.currentRow-1][Pl.currentCol] == 'X') {
+                    currentMaze[Pl.currentRow-1][Pl.currentCol] = '-';
+                    System.out.println("You have unlocked a door!");
+                }
+                else {
+                    System.out.println("There is no place nearby for the key to go into!");
+                    return;
+                }
+                break;
         }
-        else if (LVL == 2) {
-            Pl.currentRow = 0;
-            Pl.currentCol = 0;
+        if (itemSlot == 1) {
+            Pl.item1 = "";
+            if (!Pl.item2.isEmpty()) {
+                Pl.item1 = Pl.item2;
+                Pl.item2 = "";
+            }
         }
+        if (itemSlot == 2) {
+            Pl.item2 = "";
+        }
+    }
+    public void addItem(int ID, Player Pl, char[][] currentMaze) {
+        if (Pl.item2.isEmpty()) {
+            switch (ID) {
+                case 1:
+                    System.out.println("You found a Potion of Vigor!");
+                    placeInventory(Pl,currentMaze, "Potion of Vigor");
+                    break;
+                case 2:
+                    System.out.println("You found a Key");
+                    placeInventory(Pl,currentMaze,"Key");
+                    break;
+            }
+        }
+        else {
+            System.out.println("Your inventory is full!");
+        }
+    }
+    public void dropItem(Player Pl, String Name, char[][] currentMaze) {
+        switch (Name) {
+            case "Potion of Vigor":
+                currentMaze[Pl.currentRow][Pl.currentCol] = 'V';
+                System.out.println("You dropped a Potion of Vigor!");
+                break;
+            case "Key":
+                currentMaze[Pl.currentRow][Pl.currentCol] = 'K';
+                System.out.println("You dropped a Key!");
+                break;
+            default:
+                System.out.println("If this message appears, then something in the code is missing (Came from dropItem() in GameData.java)!");
+                break;
+        }
+    }
+    public void placeInventory(Player Pl, char[][] currentMaze, String name) {
+        if (Pl.item1.isEmpty()) {
+            Pl.item1 = name;
+        }
+        else {
+            Pl.item2 = name;
+        }
+        currentMaze[Pl.currentRow][Pl.currentCol] = '-';
     }
 }
