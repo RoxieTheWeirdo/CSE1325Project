c51e532c675567d8238eb7a55826800a1e24ee8c
455669 <159175072+455669@users.noreply.github.com>
2024-04-06 01:24:47 -0500

    Update Player.java

--- Maze Game Code/Player.java
+++ Maze Game Code/Player.java
@@ -25,4 +25,35 @@ public class Player {
             Pl.currentCol = 0;
         }
     }
+    //Function must be called each time player takes damage
+    public void Death(killer) {    //Checks if player is dead, killer is cause of death
+        if(health == 0) {
+        System.out.println("\t\tYou have died");
+            switch(killer) {
+            case wall:
+            System.out.println("You have hit the wall too many times");
+            break;
+            case rollingBall:
+            System.out.println("You were crushed by a large rolling ball");
+            break;
+            case fallingCeilling:
+            System.out.println("You were flattened by a falling ceiling");
+            break;
+            case minotaur:
+            System.out.println("You have been mauled by the Minotaur");
+            break;
+            }
+        Scanner continues = new Scanner(System.in);
+        System.out.println("\tType ok to continue");
+        String okInput = continues.nextLine();
+        if(okInput.equals("ok"))
+        reset();
+        }      
+    }
+    public void resetPlayer() { //player reset to starting point after death
+        System.out.println("\n\nYou awaken and see that you have returned to the same spot where you fell into the maze");
+        p1.setBaseStats(p1, p1.LVL);
+        game();
+        }
+
 }
