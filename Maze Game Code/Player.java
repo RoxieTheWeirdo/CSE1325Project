f3151aaf285e0fd6bbb25cf96c8a7684bfc8cc8f
RoxieTheWeirdo <146688788+RoxieTheWeirdo@users.noreply.github.com>
2024-04-14 20:07:18 -0500

    Thieves, Bombs, Health, and Stamina 4/14/24

--- Maze Game Code/Player.java
+++ Maze Game Code/Player.java
@@ -12,7 +12,7 @@ public class Player {
         stamina = 100;
         currentRow = 0;
         currentCol = 0;
-        LVL = 1;
+        LVL = 2;
         item1 ="";
         item2 ="";
         killer ="";
@@ -27,6 +27,36 @@ public class Player {
             Pl.currentCol = 0;
         }
     }
- 
+    public void takeDamage(double damage) {
+        health -= damage;
+        if (health <= 0) {
+            System.out.println("Player has died.");
+            // Additional death handling logic here, if needed
+        } else {
+            System.out.println("Player takes " + damage + " damage, health now: " + health);
+        }
+    }
+    public boolean isAlive() {
+        return health > 0;
+    }
+    public void useStamina(double amount) {
+        if (stamina >= amount) {
+            stamina -= amount;
+            System.out.println("Used " + amount + " stamina, remaining stamina: " + stamina);
+        } else {
+            System.out.println("Not enough stamina to perform this action!");
+        }
+    }
 
+    public void restoreStamina(double amount) {
+        stamina += amount;
+        if (stamina > 100) stamina = 100;
+        System.out.println("Stamina restored to: " + stamina);
+    }
+
+    public void restoreHealth(double amount) {
+        health += amount;
+        if (health > 100) health = 100;
+        System.out.println("Player health restored by " + amount + ", total health: " + health);
+    }
 }
